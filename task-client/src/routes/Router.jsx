8242b537e81756ed7fb9492daeb6567dc181781b
Bismark Arroyo <bis.arroyo@gmail.com>
2024-02-17 22:08:50 -0600

    add layout to routes

--- task-client/src/routes/Router.jsx
+++ task-client/src/routes/Router.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import TasksPage from '../pages/TasksPage'
 import TasksFormPage from '../pages/TasksFormPage'
+import Layout from '../components/Layout'
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Navigate to='/tasks' />} />
-        <Route path='/tasks' element={<TasksPage />} />
-        <Route path='/create' element={<TasksFormPage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Navigate to='/tasks' />} />
+          <Route path='/tasks' element={<TasksPage />} />
+          <Route path='/create' element={<TasksFormPage />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   )
 }
